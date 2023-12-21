ac48d54c715202e003ddba14f100ca1d560fb9d5
Alexander Evgin <aleasims@gmail.com>
2023-12-21 18:16:29 +0000

    Add more memory management functions
    Added `aligned_alloc`, `calloc` and `realloc`

--- libc/zk/malloc.c
+++ libc/zk/malloc.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
+
+void *aligned_alloc(size_t alignment, size_t size) {
+    return __builtin_assigner_aligned_alloc(alignment, size);
+}
+void *calloc(size_t nmemb, size_t size) {
+    return __builtin_assigner_calloc(nmemb, size);
+}
 void free(void *ptr) {
     __builtin_assigner_free(ptr);
 }
 void *malloc(size_t size) {
     return __builtin_assigner_malloc(size);
+}
+void *realloc(void *ptr, size_t size) {
+    return __builtin_assigner_realloc(ptr, size);
 }
